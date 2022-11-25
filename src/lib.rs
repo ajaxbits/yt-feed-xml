cfc370dba9d241070dfabffb272b5723a31bb9fe
Alex Jackson <contact@ajaxbits.com>
2022-11-24 20:12:51 -0600

    implement Channel

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,6 @@
-use derive_builder::Builder;
+use std::collections::HashMap;
+
+use async_trait::async_trait;
 use hyper::{
     body::{self, Buf},
     Client,
@@ -7,9 +9,9 @@ use hyper_tls::HttpsConnector;
 use rust_decimal::Decimal;
 use serde::{Deserialize, Serialize};
 
-#[derive(Serialize, Deserialize, Debug, Clone, Builder)]
+#[derive(Serialize, Deserialize, Debug, Clone)]
 #[serde(rename = "feed")]
-pub struct Feed {
+struct Feed {
     link: Vec<Link>,
     id: XmlContentString,
 
@@ -25,17 +27,55 @@ pub struct Feed {
     published: XmlContentDateTime,
 
     #[serde(rename = "entry")]
+    videos: Option<Vec<XmlVideo>>,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone, derive_builder::Builder)]
+pub struct Channel {
+    id: String,
+    title: String,
+    author: String,
+    channel_url: String,
+    published: chrono::DateTime<chrono::Utc>,
     videos: Option<Vec<Video>>,
 }
 
-impl Feed {
-    pub async fn new(id: &str) -> Self {
-        let uri = format!(
-            "https://www.youtube.com/feeds/videos.xml?channel_id={}",
-            &id
-        )
-        .parse::<hyper::http::Uri>()
-        .expect("could not parse url as Uri for some reason");
+#[derive(Serialize, Deserialize, Debug, Clone, derive_builder::Builder)]
+pub struct Playlist {
+    id: String,
+    title: String,
+    channel_id: String,
+    author: String,
+    url: String,
+    published: chrono::DateTime<chrono::Utc>,
+    videos: Option<Vec<Video>>,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+#[serde(rename_all = "camelCase")]
+pub struct Video {
+    id: String,
+    url: String,
+    channel_id: String,
+    title: String,
+    author: String,
+    author_url: String,
+    published: chrono::DateTime<chrono::Utc>,
+    updated: chrono::DateTime<chrono::Utc>,
+    group: MediaGroup,
+}
+
+#[async_trait]
+trait Generate {
+    async fn new(id: &str) -> Self;
+}
+
+#[async_trait]
+impl Generate for Feed {
+    async fn new(uri: &str) -> Self {
+        let uri = uri
+            .parse::<hyper::http::Uri>()
+            .expect("could not parse url as Uri for some reason");
 
         let https = HttpsConnector::new();
         let client = Client::builder().build::<_, hyper::Body>(https);
@@ -50,92 +90,133 @@ impl Feed {
     }
 }
 
-#[derive(Serialize, Deserialize, Debug, Clone)]
-pub struct XmlContentString {
-    #[serde(rename = "$value")]
-    pub value: String,
+impl From<XmlVideo> for Video {
+    fn from(video: XmlVideo) -> Self {
+        Video {
+            id: video.video_id.value,
+            url: video.link.href,
+            channel_id: video.channel_id.value,
+            title: video.title.value,
+            author: video.author.name.value,
+            author_url: video.author.uri.value,
+            published: video.published.value,
+            updated: video.updated.value,
+            group: video.group,
+        }
+    }
+}
+
+#[async_trait]
+impl Generate for Channel {
+    async fn new(id: &str) -> Self {
+        let uri = format!(
+            "https://www.youtube.com/feeds/videos.xml?channel_id={}",
+            &id
+        );
+
+        let feed: Feed = Feed::new(&uri).await;
+
+        let videos = feed
+            .videos
+            .map(|videos| videos.into_iter().map(Video::from).collect());
+
+        Channel {
+            id: feed.id.value,
+            title: feed.title.value,
+            author: feed.author.name.value,
+            channel_url: feed.author.uri.value,
+            published: feed.published.value,
+            videos,
+        }
+    }
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
-pub struct XmlContentu32 {
+struct XmlContentString {
     #[serde(rename = "$value")]
-    pub value: u32,
+    value: String,
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
-pub struct XmlContentDateTime {
+struct XmlContentu32 {
     #[serde(rename = "$value")]
-    pub value: chrono::DateTime<chrono::Utc>,
+    value: u32,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+struct XmlContentDateTime {
+    #[serde(rename = "$value")]
+    value: chrono::DateTime<chrono::Utc>,
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct Link {
-    pub rel: String,
-    pub href: String,
+    rel: String,
+    href: String,
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
-pub struct Author {
-    pub name: XmlContentString,
-    pub uri: XmlContentString,
-}
-
-#[derive(Serialize, Deserialize, Debug, Clone)]
-#[serde(rename_all = "camelCase")]
-pub struct Video {
-    pub id: XmlContentString,
-    pub video_id: XmlContentString,
-    pub channel_id: XmlContentString,
-    pub title: XmlContentString,
-    pub link: Link,
-    pub author: Author,
-    pub published: XmlContentDateTime,
-    pub updated: XmlContentDateTime,
-    pub group: MediaGroup,
-}
-
-#[derive(Serialize, Deserialize, Debug, Clone)]
-pub struct MediaGroup {
-    pub title: XmlContentString,
-    pub content: MediaContent,
-    pub thumbnail: MediaThumbnail,
-    pub description: Option<XmlContentString>,
-    pub community: MediaCommunity,
-}
-
-#[derive(Serialize, Deserialize, Debug, Clone)]
-pub struct MediaContent {
-    pub url: String,
-    pub r#type: String,
-    pub width: u32,
-    pub height: u32,
-}
-
-#[derive(Serialize, Deserialize, Debug, Clone)]
-pub struct MediaThumbnail {
-    pub url: String,
-    pub width: u32,
-    pub height: u32,
+struct Author {
+    name: XmlContentString,
+    uri: XmlContentString,
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
 #[serde(rename_all = "camelCase")]
-pub struct MediaCommunity {
-    pub star_rating: MediaStarRating,
-    pub statistics: MediaStatistics,
+struct XmlVideo {
+    id: XmlContentString,
+    video_id: XmlContentString,
+    channel_id: XmlContentString,
+    title: XmlContentString,
+    link: Link,
+    author: Author,
+    published: XmlContentDateTime,
+    updated: XmlContentDateTime,
+    group: MediaGroup,
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
-pub struct MediaStarRating {
-    pub count: u32,
-    pub average: Decimal,
-    pub min: u32,
-    pub max: u32,
+struct MediaGroup {
+    title: XmlContentString,
+    content: MediaContent,
+    thumbnail: MediaThumbnail,
+    description: Option<XmlContentString>,
+    community: MediaCommunity,
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
-pub struct MediaStatistics {
-    pub views: u64,
+struct MediaContent {
+    url: String,
+    r#type: String,
+    width: u32,
+    height: u32,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+struct MediaThumbnail {
+    url: String,
+    width: u32,
+    height: u32,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+#[serde(rename_all = "camelCase")]
+struct MediaCommunity {
+    star_rating: MediaStarRating,
+    statistics: MediaStatistics,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+struct MediaStarRating {
+    count: u32,
+    average: Decimal,
+    min: u32,
+    max: u32,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+struct MediaStatistics {
+    views: u64,
 }
 
 #[cfg(test)]
@@ -147,6 +228,7 @@ mod tests {
         let linus = Feed::new("UCXuqSBlHAE6Xw-yeJA0Tunw").await;
         assert_eq!(linus.id.value, "yt:channel:UCXuqSBlHAE6Xw-yeJA0Tunw");
         assert_eq!(linus.channel_id.value, "UCXuqSBlHAE6Xw-yeJA0Tunw");
+        assert_eq!(linus.title.value, "Linus Tech Tips");
     }
 
     #[tokio::test]
